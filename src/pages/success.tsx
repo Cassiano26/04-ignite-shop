bbb73fcfdc465e4a9c5eed8649b801d8ebbfd588
Cassiano26 <candidocassiano2@gmail.com>
2024-10-31 15:14:24 -0300

    style:  add success page style.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link'
+import {SuccessContainer, ImageContainer} from '../styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Cassiano Candido</strong>, sua camiseta <strong>Beyond the limits</strong> já está no seu carrinho.
+      </p>
+
+      <Link href={'/'} >
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
 }
